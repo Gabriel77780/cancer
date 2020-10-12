49ff2b3b77703ceabd589be4e0e9bd7921af6412
gabriel vasquez <gabriel77780@hotmail.com>
2020-10-12 02:40:45 -0600

    Adding icons and finishing the game :sparkles:

--- js/rules.js
+++ js/rules.js
@@ -1,48 +1,76 @@
-
-
-let player1 = {
-  turn: 0,
-  gameState: []
-}
-
-let player2 = {
-  turn: 0,
-  gameState: []
-}
-
-let currentPlay = Math.floor( Math.random() * 1) == 1 ? player1 : player2;
-
-let ticTacToeItems = document.getElementsByClassName("tic-tac-toe-item");
-
-for (i = 0; i < ticTacToeItems.length; i++) { 
-  ticTacToeItems[i].addEventListener("click", (e,r) => {
-    console.log(e,r);
-  })
- }
-
-
-
 const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
+  ["0", "1", "2"],
+  ["3", "4", "5"],
+  ["6", "7", "8"],
+  ["0", "3", "6"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["0", "4", "8"],
+  ["2", "4", "6"],
 ];
 
-function winTheGame() {
-  for (i = 0; i < winningConditions.length; i++) {
-    let a = player2.gameStateplayer1GameState.includes(winningConditions[i][0]);
-    let b = player1GameState.includes(winningConditions[i][1]);
-    let c = player1GameState.includes(winningConditions[i][2]);
+let playerOne = {
+  boardState: [],
+  icon: "assets/bulbasaur-gif.gif"
+}
 
-    if (a && b && c) {
-      alert("Alguno ganó");
-    }
+let playerTwo = {
+  boardState: [],
+  icon: "assets/charmander-gif.gif"
+}
+
+let currentPlayer = Math.floor(Math.random() * 2) == 1 ? playerOne : playerTwo;
+let totalTurn = 1;
+let endGame = false;
+
+document.querySelectorAll('.tic-tac-toe-item img').forEach(item => {
+  item.addEventListener('click', event => {
+    handleSelection(event);
+  })
+})
+
+function handleSelection(event) {
+  if (!(playerOne.boardState.includes(event.target.id) ||
+    playerTwo.boardState.includes(event.target.id)) && !endGame) {
+
+    updatePlayerBoard(event.target);
+    checkWin();
+    nextPlayer();
+
   }
+}
 
-  alert("Fin del recorrido");
+function nextPlayer() {
+
+  currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+  totalTurn = totalTurn + 1;
+
+}
+
+function updatePlayerBoard(element) {
+
+  element.src = currentPlayer.icon;
+  currentPlayer.boardState.push(element.id);
+
+}
+
+function checkWin() {
+
+  if (totalTurn < 9) {
+    for (i = 0; i < winningConditions.length; i++) {
+
+      let a = currentPlayer.boardState.includes(winningConditions[i][0]);
+      let b = currentPlayer.boardState.includes(winningConditions[i][1]);
+      let c = currentPlayer.boardState.includes(winningConditions[i][2]);
+
+      if (a && b && c) {
+        document.getElementById("winner").innerHTML = currentPlayer === playerOne ? "Jugador 1" : "Jugador 2";
+        endGame = true;
+      }
+
+    }
+  } else {
+    document.getElementById("winner").innerHTML = "Empate";
+    endGame = true;
+  }
 }
